20def9139aaf2f24dc971e7042bb22ff3b144f25
Laokaow <edwbyl-3@student.ltu.se>
2025-04-18 14:52:32 +0200

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int hi =5;
+        System.out.println(5);
+    }
+}
