f7f574031ddab804497dedfdc865acf7713d1968
Laokaow <edwbyl-3@student.ltu.se>
2025-04-18 21:01:56 +0200

    Bytte kompileringsversion till 17 istället för 23. För att förhindra att för nya funktioner används i pom.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int hi =5;
-        System.out.println(5);
+        System.out.println(hi);
     }
 }
