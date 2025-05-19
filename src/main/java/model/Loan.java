154b9a12326616a191bf6a62108dff4d20088edf
Laokaow <edwbyl-3@student.ltu.se>
2025-05-19 19:28:00 +0200

    Ändrat ganska mycket. Lagt till en overloaded konstruktor för att kunna skapa loan-objekt utan att behöva receiptId. Speciellt användbart för att slippa ändra views i MySQL.
    Även lagt till dueDate.

--- src/main/java/model/Loan.java
+++ src/main/java/model/Loan.java
@@ -9,14 +9,21 @@ public class Loan {
     private final int receiptId;
     private final LocalDate borrowDate;
     private LocalDate returnDate; //Det faktiska återlämningsdatumet
+    private LocalDate dueDate;
 
-    public Loan(int loanId, User user, Copy copy, int receiptId, LocalDate borrowDate, LocalDate returnDate) {
+    //Overloaded konstruktor. Detta för att kunna skapa lånvy utan att behöva ha Receipt med. Sätter -1 på receiptId för NULL
+    public Loan(int loanId, User user, Copy copy,  LocalDate borrowDate, LocalDate returnDate, LocalDate dueDate){
+        this(loanId, user, copy, -1, borrowDate, returnDate, dueDate);
+    }
+
+    public Loan(int loanId, User user, Copy copy, int receiptId, LocalDate borrowDate, LocalDate returnDate, LocalDate dueDate) {
         this.loanId = loanId;
         this.user = user;
         this.copy = copy;
         this.receiptId = receiptId;
         this.borrowDate = borrowDate; //Lägga till validering e.g. if(borrowDate.isAfter(returnDate)){throw new IllegalArgumentException("")}
         this.returnDate = returnDate;
+        this.dueDate = dueDate; //Lade till dueDate för att kunna visa detta i applikationen
     }
     public int getLoanId(){
         return loanId;
@@ -29,6 +36,10 @@ public class Loan {
     }
     public int getReceiptId(){
         return receiptId;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
     }
     public LocalDate getBorrowDate(){
         return borrowDate;
