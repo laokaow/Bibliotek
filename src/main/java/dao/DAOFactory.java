3a4523b7826332f81c21961622c3761dd0c03a84
Laokaow <edwbyl-3@student.ltu.se>
2025-05-20 18:16:40 +0200

    Skapat alla DAO connections för DAOFactory

--- src/main/java/dao/DAOFactory.java
+++ src/main/java/dao/DAOFactory.java
@@ -1,19 +1,71 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DAOFactory {
-    private static Connection connection;
-    private static UserDAO userDAO;
 
-    public static void init(String url, String user, String password) throws SQLException {
-        connection = DriverManager.getConnection(url, user, password);
+    private static UserDAO userDAO;
+    private static CopyDAO copyDAO;
+    private static BookDAO bookDAO;
+    private static ActorDAO actorDAO;
+    private static CategoryDAO categoryDAO;
+    private static DvdDAO dvdDAO;
+    private static JournalDAO journalDAO;
+    private static LoanDAO loanDAO;
+    private static MediaDAO mediaDAO;
+    private static ReceiptDAO receiptDAO;
+
+    public static void init(Connection connection) throws SQLException {
         userDAO = new UserDAO(connection);
+        copyDAO = new CopyDAO(connection);
+        bookDAO = new BookDAO(connection, categoryDAO);
+        actorDAO = new ActorDAO(connection);
+        categoryDAO = new CategoryDAO(connection);
+        dvdDAO = new DvdDAO(actorDAO, categoryDAO, connection);
+        journalDAO = new JournalDAO(connection);
+        loanDAO = new LoanDAO(connection);
+        mediaDAO = new MediaDAO(connection);
+        receiptDAO = new ReceiptDAO(connection);
     }
 
     public static UserDAO getUserDAO() {
         return userDAO;
+    }
+
+    public static CopyDAO getCopyDAO() {
+        return copyDAO;
+    }
+
+    public static BookDAO getBookDAO() {
+        return bookDAO;
+    }
+
+    public static ActorDAO getActorDAO() {
+        return actorDAO;
+    }
+
+    public static CategoryDAO getCategoryDAO() {
+        return categoryDAO;
+    }
+
+    public static DvdDAO getDvdDAO() {
+        return dvdDAO;
+    }
+
+    public static JournalDAO getJournalDAO() {
+        return journalDAO;
+    }
+
+    public static LoanDAO getLoanDAO() {
+        return loanDAO;
+    }
+
+    public static MediaDAO getMediaDAO() {
+        return mediaDAO;
+    }
+
+    public static ReceiptDAO getReceiptDAO() {
+        return receiptDAO;
     }
 }
